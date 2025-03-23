3f70c3e6767285a08cece5143ccb2a0b35612a7f
CalebCouch <calebblackcat@gmail.com>
2025-03-23 14:10:50 -0400

    test

--- proc/target/debug/deps/proc-dc9b6f32225d4dea.d
+++
@@ -1,9 +0,0 @@
-/home/orange/rust_on_rails/proc/target/debug/deps/libproc-dc9b6f32225d4dea.rmeta: src/lib.rs Cargo.toml
-
-/home/orange/rust_on_rails/proc/target/debug/deps/proc-dc9b6f32225d4dea.d: src/lib.rs Cargo.toml
-
-src/lib.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
