80a62395330047aae5f0de9a635731904036718d
CalebCouch <calebblackcat@gmail.com>
2025-04-24 13:18:08 -0400

    test

--- src/components/drawable.rs
+++ src/components/drawable.rs
@@ -47,7 +47,6 @@ pub(crate) trait _Drawable: Debug {
 impl _Drawable for Text {
     fn request_size(&self, ctx: &mut Context) -> RequestBranch {
         let size = self.size(ctx.base_context.render_ctx());
-        println!("size: {:?}", size);
         RequestBranch(SizeRequest::fixed(size), vec![])
     }
 
