262741ed69d2c1775504b1f687a8bd921b4a538f
Ella Mae <ella@orange.me>
2025-05-29 13:27:16 -0400

    wip

--- src/base/driver/share.rs
+++ src/base/driver/share.rs
@@ -1,41 +1,57 @@
 #[cfg(target_os = "ios")]
-use objc2::{class, msg_send};
+use objc2::{class, msg_send, sel};
 #[cfg(target_os = "ios")]
-use objc2_foundation::{NSArray, NSString, NSObject};
+use objc2_foundation::{NSArray, NSString, NSObject, NSData};
 #[cfg(target_os = "ios")]
 use objc2::rc::autoreleasepool;
+#[cfg(target_os = "ios")]
+use objc2::rc::Id;
+#[cfg(target_os = "ios")]
+use objc2::rc::AutoreleasePool;
 
 pub struct Share;
 
 impl Share {
     #[cfg(target_os = "ios")]
+    
     pub fn share(text: &str) {
         autoreleasepool(|_| {
             let ns_string = NSString::from_str(text);
-            let items = NSArray::from_slice(&[&*ns_string]);
-    
-            let cls = class!(UIActivityViewController);
-            let activity_controller: *mut NSObject = unsafe { msg_send![cls, alloc] };
-    
-            let activity_controller: *mut NSObject = unsafe {
-                msg_send![activity_controller, initWithActivityItems:&*items, applicationActivities: std::ptr::null_mut::<NSArray<NSObject>>()]
+            let ns_string_obj: Id<NSObject> = unsafe { std::mem::transmute(ns_string) };
+
+            let image_name = NSString::from_str("ShareIcon");
+            let image: *mut NSObject = unsafe { msg_send![class!(UIImage), imageNamed: &*image_name] };
+            let image_obj: Id<NSObject> = unsafe {
+                Id::retain(image.cast()).expect("Image 'ShareIcon' not found in asset catalog")
             };
-    
-            let ui_app = class!(UIApplication);
-            let shared_app: *mut NSObject = unsafe { msg_send![ui_app, sharedApplication] };
-            let key_window: *mut NSObject = unsafe { msg_send![shared_app, keyWindow] };
-            let root_vc: *mut NSObject = unsafe { msg_send![key_window, rootViewController] };
-    
+
+            let items = NSArray::from_slice(&[&*ns_string_obj, &*image_obj]);
+
+            let activity_controller: *mut NSObject = unsafe {
+                let cls = class!(UIActivityViewController);
+                let alloc: *mut NSObject = msg_send![cls, alloc];
+                msg_send![
+                    alloc,
+                    initWithActivityItems: &*items,
+                    applicationActivities: std::ptr::null_mut::<NSArray<NSObject>>()
+                ]
+            };
+
+            let app: *mut NSObject = unsafe { msg_send![class!(UIApplication), sharedApplication] };
+            let window: *mut NSObject = unsafe { msg_send![app, keyWindow] };
+            let root_vc: *mut NSObject = unsafe { msg_send![window, rootViewController] };
+
             let _: () = unsafe {
                 msg_send![
                     root_vc,
                     presentViewController: activity_controller,
                     animated: true,
-                    completion: std::ptr::null_mut::<objc2::runtime::AnyObject>()
+                    completion: std::ptr::null_mut::<NSObject>()
                 ]
             };
         });
     }
+
     
     #[cfg(target_os = "macos")]
     pub fn share(text: &str) {}
